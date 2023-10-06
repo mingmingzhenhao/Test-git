70fb10ebbe91b3467df38a12779a9dca8d6d0d1d
常明明 <342214462@qq.com>
2023-10-06 11:26:59 +0800

    第一次修改

---
+++ src/main/java/com/powernode/Main.java
@@ -0,0 +1,36 @@
+package com.powernode;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        Student student = new Student();
+        student.setName("明明");
+        student.setNo(1);
+        System.out.println(student);
+
+    }
+}
+class Student{
+    int no;
+    String name;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
